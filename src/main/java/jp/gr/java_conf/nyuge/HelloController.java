ecab8a502442c3a95fd427a3b24841a8fcc2c30b
nyuge <aiben.mail1@gmail.com>
2018-10-28 01:43:26 +0900

    Set a basic thymeleaf template

--- src/main/java/jp/gr/java_conf/nyuge/HelloController.java
+++ src/main/java/jp/gr/java_conf/nyuge/HelloController.java
@@ -1,16 +1,29 @@
 package jp.gr.java_conf.nyuge;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @EnableAutoConfiguration
 public class HelloController {
-    @RequestMapping("/")
-    @ResponseBody
-    public String index() {
-        return "Hello world";
+
+//    @RequestMapping({"/"})
+//    public String index(HttpSession session) {
+//        session.setAttribute("mySessionAttribute", "someValue");
+//
+//        return "index";
+//    }
+
+    @GetMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView mav = new ModelAndView("hello");
+        mav.addObject("message", "Hello Thymeleaf!!");
+
+        return mav;
     }
 }
